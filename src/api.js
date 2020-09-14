20eb646b548d2852a01a345cfdba17d29bea616f
devfernandoabreu <devfernandoabreu@gmail.com>
2020-09-13 22:37:31 -0300

    Criando o login criar

--- src/api.js
+++ src/api.js
@@ -25,7 +25,6 @@ export function TOKEN_VALIDATE_POST(token) {
   };
 }
 
-
 export function USER_GET(token) {
   return {
     url: API_URL + '/api/user',
@@ -35,5 +34,18 @@ export function USER_GET(token) {
         Authorization: 'Bearer ' + token,
       },
     },
+  };
+}
+
+export function USER_POST(body) {
+  return {
+    url: API_URL + '/api/user',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body)
+    }
   };
 }
