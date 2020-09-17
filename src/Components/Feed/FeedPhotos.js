db21f92cc69b52d8b0a9f18fc7b7659935caac74
devfernandoabreu <devfernandoabreu@gmail.com>
2020-09-17 12:08:04 -0300

    Feed Modal parte 1

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
@@ -17,13 +17,13 @@ const FeedPhotos = () => {
     }    
     if (!data) fetchPhotos();
   }, [request]);
-  if(error) return <Error error={error} />
-  if(loading) return <Loading />
+  if(error) return <Error error={error} />;
+  if(loading) return <Loading />;
   if (data)
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) =>(
-          <FeedPhotosItem key={photo.id} photo={photo} />
+          <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto} />
         ))}
       </ul>
     );
