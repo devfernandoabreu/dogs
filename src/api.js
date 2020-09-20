9c785e4b51c4238cbeab13bf9bc52022de1457de
devfernandoabreu <devfernandoabreu@gmail.com>
2020-09-20 15:12:59 -0300

    Criando o formulário de resetar a senha

--- src/api.js
+++ src/api.js
@@ -122,3 +122,16 @@ export function PASSWORD_LOST(body) {
   }
 }
 
+export function PASSWORD_RESET(body) {
+  return {
+    url: API_URL + '/api/password/reset',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }  
+  }
+}
+
