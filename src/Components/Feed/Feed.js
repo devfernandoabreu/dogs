a73e824b1c101c8e346b005ac73077369e2b158f
devfernandoabreu <devfernandoabreu@gmail.com>
2020-09-20 12:31:29 -0300

    Adicionando protoTypes e criando o componente de erro

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
+import PropTypes from 'prop-types';
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -36,13 +37,37 @@ const Feed = ({ user }) => {
       {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />}
       {pages.map((page) => (
         <FeedPhotos 
+          key={page}
           user={user} 
           page={page}
           setModalPhoto={setModalPhoto}
           setInfinite={setInfinite}
         />
       ))}
+      {!infinite && !user && (
+        <p
+          style={{
+            textAlign: 'center',
+            padding:'2rem 0 4rem 0',
+            color: '#888',
+          }}
+        >
+          Não existem mais postagens
+        </p>
+      )}
     </div>
   );
 };
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
+}
+
 export default Feed;
